219089cde8553e3dac90f93ca5602150027eee49
kartik-04 <04kartik04@gmail.com>
2025-05-20 13:52:12 +0530

    made changes to promise.all.polyfill.js

--- Class21/Promise.all.Polyfill.js
+++ Class21/Promise.all.Polyfill.js
@@ -1,21 +1,35 @@
-Promise.myAll = function (promises) {
-  return new Promise((resolve, reject) => {
-    let responses = [];
+Promise.myAllSettled = function (promises) {
+  return new Promise((resolve) => {
+    if (!Array.isArray(promises)) {
+      throw new TypeError("Input must be an array of promises");
+    }
+
+    let results = [];
     let responseCounter = 0;
+
     promises.forEach((promise, idx) => {
-      promise
+      Promise.resolve(promise)
         .then((res) => {
-          //successful
-          responses[idx] = res;
-          responseCounter++;
-          if (responseCounter === promises.length) {
-            resolve(responses);
-          }
+          results[idx] = { status: "fulfilled", value: res };
         })
         .catch((err) => {
-          //Unsuccessful
-          reject(err);
+          results[idx] = { status: "rejected", reason: err };
+        })
+        .finally(() => {
+          responseCounter++;
+          if (responseCounter === promises.length) {
+            resolve(results);
+          }
         });
     });
   });
 };
+
+// Example usage
+const promise1 = Promise.resolve(10);
+const promise2 = Promise.reject("Error occurred");
+const promise3 = Promise.resolve(30);
+
+Promise.myAllSettled([promise1, promise2, promise3]).then((results) =>
+  console.log("Results:", results)
+);
