2a1cd991ce2c710b43076a4a035c6ece2d2f550f
kartik-04 <04kartik04@gmail.com>
2025-05-20 14:13:05 +0530

    chagne promise.all

--- Class21/Promise.all.Polyfill.js
+++ Class21/Promise.all.Polyfill.js
@@ -1,25 +1,21 @@
-Promise.myAllSettled = function (promises) {
-  return new Promise((resolve) => {
-    if (!Array.isArray(promises)) {
-      throw new TypeError("Input must be an array of promises");
-    }
-
-    let results = [];
+Promise.myAll = function (promises) {
+  return new Promise((resolve, reject) => {
+    let responses = [];
     let responseCounter = 0;
 
     promises.forEach((promise, idx) => {
-      Promise.resolve(promise)
+      Promise.resolve(promise) // Ensure the input is a promise
         .then((res) => {
-          results[idx] = { status: "fulfilled", value: res };
-        })
-        .catch((err) => {
-          results[idx] = { status: "rejected", reason: err };
-        })
-        .finally(() => {
+          // Successful
+          responses[idx] = res;
           responseCounter++;
           if (responseCounter === promises.length) {
-            resolve(results);
+            resolve(responses);
           }
+        })
+        .catch((err) => {
+          // Unsuccessful
+          reject(err);
         });
     });
   });
@@ -27,9 +23,9 @@ Promise.myAllSettled = function (promises) {
 
 // Example usage
 const promise1 = Promise.resolve(10);
-const promise2 = Promise.reject("Error occurred");
+const promise2 = Promise.resolve(20);
 const promise3 = Promise.resolve(30);
 
-Promise.myAllSettled([promise1, promise2, promise3]).then((results) =>
-  console.log("Results:", results)
-);
+Promise.myAll([promise1, promise2, promise3])
+  .then((results) => console.log("Results:", results)) // Output: Results: [10, 20, 30]
+  .catch((error) => console.error("Error:", error));
